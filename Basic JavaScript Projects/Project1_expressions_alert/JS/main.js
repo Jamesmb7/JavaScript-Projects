2083ea716c24ac1f7733e3e0f0eee68897052d55
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-06-18 01:06:09 -0500

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+
+/***Variables***/
+var A = "This is a string." // This makes variable A a string
+var B = "Hello, World!" //This assigns variable B a string to say "Hello, World!"
+var C = "<br><br>I thought the question \"What do you do if you need to display a backslash in your code? Write two of them! \\\" was very funny."
+var D = "<br><br>I had some a few thoughts like \"How do you pronounce concatenate?\"" + "\"Why is the \+ sign actually used between quotes?\""+ "\"Oh well I'll just follow along, I'm sure i'll understand eventually.\" "
+var E = "<br><br>Concatenated" + " String"
+var Family = "<br><br>The Bennetts", Dad = "<br><br>Darrel", Mom = "<br><br>Trena", Son = "<br><br>James";
+
+/***End Variables***/
+
+
+document.write(A); //This calls Variable A to write "This is a string."
+window.alert(B); // This creates an alert that pops up that says "Hello, World!"
+document.write(C); // This calls variable C
+document.write(D); //This calls variable D
+document.write(E); //This calls variable E
+document.write(Dad); //This calls variable Dad
+document.write(Mom+Son);   //This calls variable Mom and Son
