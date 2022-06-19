93e37a26c040948a3877bba1ad1b70cc202ceadf
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-06-18 23:30:03 -0500

    Create main.js

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+var A = 10
+var B = 5
+
+
+
+function My_First_Function() //Name of the function being called
+{
+    document.write(A*B); //multiplies variable A and variable B to return an answer of 50. 
+    document.getElementById("Button_Text").innerHTML = str; //ID assigned to the P element to cause the change
+}
+
+function myFunction()//Name of the function being called
+{
+    var sentence = "I am struggling"; //first half of a complete sentence called to replace the original p text
+    sentence += " to understand these functions!"; // second half of a complete sentence called to replace the original p text
+    document.getElementById("Concatenate").innerHTML = sentence;//ID assigned to the P element to cause the change
+}
+
