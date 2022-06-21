ddf6d271c85712fab74b2f6be32235ee3aa6424c
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-06-20 22:39:48 -0500

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,21 +1,40 @@
+//Document.write elements/
+
 document.write(typeof "Word"); //will write "string"
-
+document.write("<br>"); //breaks in js (starts a new line)
 document.write(typeof 3); // will write "number"
+document.write("<br>") //breaks in js (starts a new line)
+document.write("35" + 5); //will put these numbers together to be 355
+document.write("<br>") //breaks in js (starts a new line)
+document.write(2E310); //will write "infinity"
+document.write("<br>") //breaks in js (starts a new line)
+document.write(-3e310); //will write "-infinity"
 
-document.write("35" + 5);
+//Functions/ 
 
 function my_function()
 {
-    document.getElementById("Test").innerHTML = 0/0;
+    document.getElementById("Test").innerHTML = 0/0; //NaN ("Not a Number")
 }
 
 function my_function1()
 {
-    document.getElementById("Test1").innerHTML = isNaN('This is a string');
+    document.getElementById("Test1").innerHTML = isNaN('This is a string'); //Will be true (this is a string, it's not a number)
 }
 
 function my_function2()
 {
-    document.getElementById("Test2").innerHTML = isNaN('007');
+    document.getElementById("Test2").innerHTML = isNaN('007'); //Will be false (This is a number)
 }
-// Why don't these work at all? What am I missing here?
+
+function infinity()
+{
+    document.getElementById("Infinity").innerHTML = document.write(3e310);//will display infinity
+}
+
+function not_infinity()
+{
+    document.getElementById("not_Infinity").innerHTML = document.write(-3e310); //will display -infinity
+}
+//personal note - how would you not overwrite everything else when doing infinity and -infinity here?
+
