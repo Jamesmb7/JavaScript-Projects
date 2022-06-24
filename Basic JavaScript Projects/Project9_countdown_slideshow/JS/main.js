3086a0b852c8d9c0eb86f664cfdacb14848e8ea0
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-06-24 06:47:30 -0500

    Update main.js

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,55 @@
+function countdown()
+{
+    var seconds = document.getElementById("seconds").value;
+
+    function tick()
+    {
+        seconds=seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1)
+        {
+            alert("Time's up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+//should I know all of this and how to create something like this at this point already?
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n)
+{
+    showSlides(slideIndex += n);
+}
+
+//Thumbnail image controls
+function currentSlide(n)
+{
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n>slides.length) {slideIndex=1}
+    if (n<1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++)
+    {
+        slides[i].style.display= "none";
+    }
+    for (i=0; i < dots.length; i++)
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+//could use a walkthrough on how each of these above lines of code correlate to the html/css.  
