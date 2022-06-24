b8ca99ef194080e4d2e1647b53416718d9a8d074
Jamesmb7 <106565960+Jamesmb7@users.noreply.github.com>
2022-06-24 09:38:16 -0500

    Create main.js

---
+++ Basic JavaScript Projects/Async_and_Defer_Assignment/JS/main.js
@@ -0,0 +1,8 @@
+alert(document.getElementById("p1").innerHTML);
+
+function defer_Assign()
+{
+    var sentence = "This is for my defer assignment";
+    var section = sentence.toUpperCase();
+    document.getElementById("clicks").innerHTML=section;
+}
